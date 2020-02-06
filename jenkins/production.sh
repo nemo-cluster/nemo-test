f401392c33757927c608193bd48e6040a70e87e5
Thiago Abdo <tjabdo@inf.ufpr.br>
2020-02-06 14:32:46 +0100

    test
    Signed-off-by: Thiago Abdo <tjabdo@inf.ufpr.br>

---
+++ jenkins/production.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+
+# name of the script withouth the path
+scriptname=$(basename "$0")
+# path to the folder containing the script
+scriptdir=$(dirname "$0")
+
+usage() {
+    echo -e "\n Usage: $0 [OPTIONS] -l <list> -p <prefix>
+    -h,--help         Help message
+    -l,--list         Absolute path to production file   (mandatory: EasyBuild production list)
+    -p,--prefix       Absolute path to EasyBuild prefix  (mandatory: installation folder)
+    -u,--unuse        Module unuse colon separated PATH  (optional: default is null)
+    --hide-deps       Force hide modules listed in 'hide-deps' (TestingEB only)
+    --exit-on-error   Exit when an error occurs (TestingEB only)
+    "
+    exit 1;
+}
+
+longopts="help,list:,prefix:,unuse:,hide-deps,exit-on-error"
+shortopts="h,l:,p:,u:"
+eval set -- $(getopt -o ${shortopts} -l ${longopts} -n ${scriptname} -- "$@" 2> /dev/null)
+
+eb_files=()
+eb_lists=()
+while [ $# -ne 0 ]; do
+    case $1 in
+        -f | --force)
+            shift
+            force_list="$1"
+            ;;
+        -h | --help)
+            usage
+            ;;
+        -l | --list)
+            shift
+            mapfile -O ${#eb_files[@]} -t eb_files < $1
+            eb_lists+=($1)
+            ;;
+        -p | --prefix)
+            shift
+            PREFIX="$1"
+            ;;
+        -u | --unuse)
+            shift
+            unuse_path="$1"
+            ;;
+        --exit-on-error)
+            exit_on_error=true
+            ;;
+        --hide-deps)
+            hidden_deps=true
+            ;;
+        --)
+            ;;
+        *)
+            usage
+            ;;
+    esac
+    shift
+done
+
+
+# optional EasyBuild arguments
+eb_args=""
+
+# --- COMMON SETUP ---
+# module unuse PATH before loading EasyBuild module and building
+# When building on testing and regression we should unuse the path???? or just install/modules path resolves it?
+# No need to unuse a path, we can just change the install-modules path.......
+
+if [ -n "$unuse_path" ]; then
+ echo -e " Unuse path: $unuse_path "
+ module unuse $unuse_path
+ echo -e " Updated MODULEPATH: $MODULEPATH "
+fi
+
+# check prefix folder
+if [ -z "$PREFIX" ]; then
+    echo -e "\n Prefix folder not defined. Please use the option -p,--prefix to define the prefix folder \n"
+    usage
+else
+ export EASYBUILD_PREFIX=$PREFIX
+# check if PREFIX is already in MODULEPATH after unuse command
+ statuspath=$(echo "$MODULEPATH" | grep -c "$EASYBUILD_PREFIX")
+ if [ "$statuspath" -eq 0 ]; then
+  echo -e " Use path (EASYBUILD_PREFIX): $EASYBUILD_PREFIX/modules/all "
+  module use "$EASYBUILD_PREFIX/modules/all"
+  echo -e " Updated MODULEPATH: $MODULEPATH "
+ fi
+fi
+
+# --- BUILD ---
+# load module EasyBuild-custom
+module load EasyBuild
+
+# add hidden flag # dont realy understand this shit...
+if [ -n "${eb_lists}" ] && [ -n "${hidden_deps}" ]; then
+  __eb_list=$(eb --show-full-config | grep -i hide | awk -F'=' '{print $2}' | head -1)
+  IFS=', ' read -r -a hidden_deps <<< "${__eb_list}"
+
+# match  items with hide deps list: matching items will be built using the EasyBuild flag '--hidden'
+ echo -e "Items matching hidden list and easybuild recipes to install (\"${eb_lists}\")"
+ for item in "${hidden_deps[@]}"; do
+     hidden_match=$(grep $item "${eb_lists[@]}")
+     if [ -n "${hidden_match}" ]; then
+# 'grep -n' returns the 1-based line number of the matching pattern within the input file
+         index_list=$(cat "${eb_lists[@]}" | grep -n "${item}" | awk -F ':' '{print $(NF-1)-1}')
+# append the --hidden flag to matching items within the selected build list
+         for index in ${index_list}; do
+             eb_files[$index]+=" --hidden"
+             echo "${eb_files[$index]}"
+         done
+     fi
+ done
+fi
+
+# print EasyBuild configuration, module list, production file(s), list of builds
+echo -e "\n EasyBuild version and configuration ('eb --version' and 'eb --show-config'): "
+echo -e " $(eb --version) \n $(eb --show-config) \n"
+echo -e " Modules loaded ('module list -t'): "
+echo -e " $(module list -t)"
+echo -e " Compilation file: " "${eb_lists[@]}" "\n"
+echo -e " List of builds (including options):"
+for ((i=0; i<${#eb_files[@]}; i++)); do
+# use eval to expand environment variables in the EasyBuild options of each build
+    eb_files[i]=$(eval echo "${eb_files[i]}")
+    echo "${eb_files[$i]}"
+done
+
+# checks dependency list using dry run
+dryrun=$(eb "${eb_files[@]}" -Dr ${eb_args} 2>&1)
+if [[ "$dryrun" =~ "ERROR" ]]; then
+ echo -e "$dryrun" | grep "ERROR"
+ exit 1
+fi
+
+# start time
+echo -e "\n Starting builds on $(date)"
+starttime=$(date +%s)
+# compile software with prefix, with
+
+
+
+echo "COMPILING!!!"
+
+
+# end time
+endtime=$(date +%s)
+# time difference
+difftime=$((endtime-starttime))
+# convert seconds to hours minutes seconds format
+ h=$((difftime/3600))
+ m=$(( (difftime%3600) /60))
+ s=$((difftime%60))
+echo -e "\n Builds ended on $(date) (elapsed time is $difftime s : ${h}h ${m}m ${s}s) \n"
+
+# cumulative exit status of all the builds and the last command
+exit $((status+$?))
