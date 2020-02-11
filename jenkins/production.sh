be55dcfdbf2829e3e75678740253a0912e729506
Thiago Abdo <tjabdo@inf.ufpr.br>
2020-02-11 16:31:52 +0100

    building
    Signed-off-by: Thiago Abdo <tjabdo@inf.ufpr.br>

--- jenkins/production.sh
+++ jenkins/production.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+# shellcheck disable=SC2145
 # name of the script withouth the path
 scriptname=$(basename "$0")
 # path to the folder containing the script
@@ -11,6 +11,7 @@ usage() {
     -h,--help         Help message
     -l,--list         Absolute path to production file   (mandatory: EasyBuild production list)
     -p,--prefix       Absolute path to EasyBuild prefix  (mandatory: installation folder)
+    -r, --robot       Robot path that is going to be used
     -u,--unuse        Module unuse colon separated PATH  (optional: default is null)
     --hide-deps       Force hide modules listed in 'hide-deps' (TestingEB only)
     --exit-on-error   Exit when an error occurs (TestingEB only)
@@ -18,8 +19,8 @@ usage() {
     exit 1;
 }
 
-longopts="help,list:,prefix:,unuse:,hide-deps,exit-on-error"
-shortopts="h,l:,p:,u:"
+longopts="help,list:,prefix:,robot:,unuse:,hide-deps,exit-on-error"
+shortopts="h,l:,p:,r:,u:"
 eval set -- $(getopt -o ${shortopts} -l ${longopts} -n ${scriptname} -- "$@" 2> /dev/null)
 
 eb_files=()
@@ -42,6 +43,10 @@ while [ $# -ne 0 ]; do
             shift
             PREFIX="$1"
             ;;
+        -r | --robot)
+            shift
+            ROBOT="$1"
+            ;;
         -u | --unuse)
             shift
             unuse_path="$1"
@@ -63,7 +68,7 @@ done
 
 
 # optional EasyBuild arguments
-eb_args=""
+eb_args=()
 
 # --- COMMON SETUP ---
 
@@ -72,8 +77,13 @@ if [ -z "$PREFIX" ]; then
     echo -e "\n Prefix folder not defined. Please use the option -p,--prefix to define the prefix folder \n"
     usage
 else
-  eb_args="--prefix=$PREFIX"
+  eb_args+=("--prefix=$PREFIX ")
 fi
+
+if [ -n "$ROBOT" ]; then
+  eb_args+=("--robot=$ROBOT")
+fi
+
 
 # --- BUILD ---
 module use $HOME/easybuild/modules/all
@@ -102,9 +112,9 @@ fi
 
 # print EasyBuild configuration, module list, production file(s), list of builds
 echo -e "\n EasyBuild version and configuration ('eb --version' and 'eb --show-config'): "
-echo -e " $(eb --version) \n $(eb --show-config) \n"
-echo -e " Modules loaded ('module list -t'): "
-echo -e " $(module list -t)"
+echo -e " $(eb --version) \n $(eb --show-config ${eb_args[@]}) \n"
+echo -e " Modules loaded ('module list'): "
+echo -e " $(module list)"
 echo -e " Compilation file: " "${eb_lists[@]}" "\n"
 echo -e " List of builds (including options):"
 for ((i=0; i<${#eb_files[@]}; i++)); do
@@ -114,7 +124,8 @@ for ((i=0; i<${#eb_files[@]}; i++)); do
 done
 
 # checks dependency list using dry run
-dryrun=$(eb "${eb_files[@]}" -Dr ${eb_args} 2>&1)
+echo -e eb "${eb_files[@]}" -Dr "${eb_args[@]}"
+dryrun=$(eb "${eb_files[@]}" -Dr "${eb_args[@]}" 2>&1)
 if [[ "$dryrun" =~ "ERROR" ]]; then
  echo -e "$dryrun" | grep "ERROR"
  exit 1
@@ -130,8 +141,8 @@ for((i=0; i<${#eb_files[@]}; i++)); do
   # define name and version of the current build starting from the recipe name (obtained removing EasyBuild options from eb_files)
   recipe=$(echo "${eb_files[$i]}" | cut -d' ' -f 1)
   name=$(echo "$recipe" | cut -d'-' -f 1)
-  echo -e "eb ${eb_files[$i]} -r ${eb_args}"
-  eb "${eb_files[$i]}" -r ${eb_args}
+  echo -e eb "${eb_files[$i]}" -r "${eb_args[@]}"
+  eb "${eb_files[$i]}" -r "${eb_args[@]}"
   status=$((status+$?))
 done
 
